82d46075cef78ac5442298b052f2163987d01208
jose <jose12_bernal@hotmail.com>
2024-03-12 18:19:56 -0600

    port added

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: process.env.DB_HOST
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT
 })
 
 export default sequelize
