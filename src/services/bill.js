cbbdceecabff7a30ca78b20c526557b687ec553e
jose <jose12_bernal@hotmail.com>
2024-03-18 16:25:02 -0600

    generate bill fixed

--- src/services/bill.js
+++ src/services/bill.js
@@ -1,12 +1,14 @@
 import billModel from "../model/BillModel.js"
+import tableModel from "../model/TableModel.js"
 
-export const generateBill = async (products, ticketId, tableId, userId) => {
+export const generateBill = async (ticketId, tableId, userId) => {
   try {
 
     if (!ticketId || !tableId || !userId) {
       return 'error al generar facturas (te falta datos por proporcionar)'
     }
-    const newBill = await billModel.create({ products, ticketId, tableId, userId })
+    const newBill = await billModel.create({ ticketId, tableId, userId })
+    await tableModel.findByIdAndUpdate(tableId, { available: true }, { new: true })
     if (!newBill) {
       return 'error al generar la factura'
     }
@@ -18,7 +20,7 @@ export const generateBill = async (products, ticketId, tableId, userId) => {
 
 export const getBills = async () => {
   try {
-    const bill = await billModel.find()
+    const bill = await billModel.find().populate('ticketId').populate('tableId')
     if (!bill) {
       return 'no hay facturas'
     }
