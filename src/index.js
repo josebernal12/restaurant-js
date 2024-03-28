27ad8351eee5157d4dd8d77e1bd5cb9267c3367c
jose <jose12_bernal@hotmail.com>
2024-03-28 09:28:38 -0700

    fixed cors

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const corsOptions = {
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
-app.use(corsOptions)
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
