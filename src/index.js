63a02bc5a7da434d7f637a7bbb2d06f97868daa1
jose <jose12_bernal@hotmail.com>
2024-04-03 13:35:05 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import noteRouter from './router/note.js'
 const app = express()
 const port = process.env.PORT || 8080
 const corsOptions = {
-  origin: ['http://localhost:5173/api', 'http://localhost:5174/api', 'http://127.0.0.1:5174/api', 'http://127.0.0.1:5173/api'],
+  origin: ['http://localhost:5173/', 'http://localhost:5174/', 'http://127.0.0.1:5174/', 'http://127.0.0.1:5173/'],
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
