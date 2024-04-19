710f65456d727f38761712d2dccb0db85268591a
jose <jose12_bernal@hotmail.com>
2024-04-19 17:27:03 -0600

    best products

--- src/services/products.js
+++ src/services/products.js
@@ -175,7 +175,6 @@ export const bestProduct = async (range) => {
       switch (range) {
         case TimeRange.DAY:
           // Rango para el día actual
-          console.log('entreee')
           const today = new Date();
           startDate = new Date(today);
           startDate.setHours(0, 0, 0, 0); // Comienzo del día
@@ -224,22 +223,24 @@ export const bestProduct = async (range) => {
 
     // Recorrer todas las facturas
     bills.forEach(bill => {
-      // Verificar si bill.ticketId está definido y si tiene productos
-      if (bill.ticketId && bill.ticketId.products && bill.ticketId.products.length > 0) {
-        // Recorrer todos los productos de la factura
-        bill.ticketId.products.forEach(product => {
-          // Verificar si el producto tiene un ID válido y una cantidad
-          if (product._id && product.stock) {
-            // Si el producto ya está en el objeto, aumenta la cantidad vendida
-            if (soldProducts[product._id]) {
-              soldProducts[product._id].stock += product.stock;
-            } else {
-              // Si el producto no está en el objeto, inicializa la cantidad vendida
-              soldProducts[product._id] = { stock: product.stock };
+      // Verificar si bill.ticketId está definido
+      bill.ticketId.forEach(value => {
+        if (bill.ticketId && value.products) {
+          // Recorrer todos los productos de la factura
+          value.products.forEach(product => {
+            // Si el producto tiene un ID
+            if (product._id) {
+              // Si el producto ya está en el objeto, aumenta la cantidad vendida
+              if (soldProducts[product._id]) {
+                soldProducts[product._id].stock += product.stock;
+              } else {
+                // Si el producto no está en el objeto, inicializa la cantidad vendida
+                soldProducts[product._id] = { stock: product.stock };
+              }
             }
-          }
-        });
-      }
+          });
+        }
+      })
     });
 
     // Obtener los nombres de los productos más vendidos
@@ -253,6 +254,13 @@ export const bestProduct = async (range) => {
     // Ordenar los productos por la cantidad vendida (de mayor a menor)
     const sortedProducts = productsInfo.sort((a, b) => b.stock - a.stock);
 
+    // Verificar si no hay productos disponibles
+    if (sortedProducts.length === 0) {
+      return {
+        products: []
+      }
+    }
+
     // Devolver los nombres y cantidades de los productos más vendidos
     return sortedProducts;
   } catch (error) {
@@ -260,7 +268,6 @@ export const bestProduct = async (range) => {
     throw new Error('Ocurrió un error al obtener los productos más vendidos.');
   }
 };
-
 
 
 
