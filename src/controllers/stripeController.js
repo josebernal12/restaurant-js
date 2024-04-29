8fd6be65c05fe69f1ecfd98e6f9ca2a84e817fad
jose bernal <jose12_bernal@hotmail.com>
2024-04-29 12:07:51 -0600

    without description

--- src/controllers/stripeController.js
+++ src/controllers/stripeController.js
@@ -1,11 +1,10 @@
 import { stripe } from "../stripe/stripe.js"
 
 export const stripeController = async (req, res) => {
-    const { id, amount, description } = req.body
+    const { id, amount } = req.body
     const payment = await stripe.paymentIntents.create({
         amount,
         currency: 'MXN',
-        description,
         payment_method: id,
         confirm: true
     })
