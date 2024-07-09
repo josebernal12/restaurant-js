b9ab2b3756f946eb68cf39065d32edc53b05f8e1
jose bernal <jose12_bernal@hotmail.com>
2024-07-08 17:02:46 -0700

    change

--- src/index.js
+++ src/index.js
@@ -32,16 +32,16 @@ app.use(express.json())
 app.use(morgan('combined'))
 app.use(compression())
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	// store: ... , // Redis, Memcached, etc. See below.
+// })
 
-// Apply the rate limiting middleware to all requests.
-app.use(limiter)
+// // Apply the rate limiting middleware to all requests.
+// app.use(limiter)
 
 
 app.use('/api/user', userRouter)
