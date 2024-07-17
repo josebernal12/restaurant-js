f153b3059e3140e2e8b43820c0865e0da7856480
jose bernal <jose12_bernal@hotmail.com>
2024-07-17 11:41:13 -0700

    change

--- src/controllers/methodOfPaymentController.js
+++ src/controllers/methodOfPaymentController.js
@@ -7,7 +7,7 @@ export const updateMethodOfPaymentController = async (req, res) => {
 
     const { id } = req.params
     const { cash, transfer, creditCard, creditDebit } = req.body
-    const methodOfPayment = await updateMethodOfPayment(id, cash, transfer, creditCard, creditDebit)
+    const methodOfPayment = await updateMethodOfPayment(req.user.companyId.toString(), cash, transfer, creditCard, creditDebit)
     if (methodOfPayment?.msg) {
         return res.status(400).json(methodOfPayment)
     }
